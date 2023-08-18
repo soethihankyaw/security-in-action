7d344f22925a379aff6090da5a88f6c43189f3db
SoeThiHanKyaw <soethiha251@gmail.com>
2023-08-18 09:17:58 +0630

    Second Commit

--- spring-security-in-action/src/main/java/com/spring/config/ProjectConfig.java
+++ spring-security-in-action/src/main/java/com/spring/config/ProjectConfig.java
@@ -2,33 +2,48 @@ package com.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-public class ProjectConfig {
+@EnableWebSecurity
+public class ProjectConfig{
 	
 	@Bean
 	public UserDetailsService userDetailsService() {
 		
 		var userDetailsService = new InMemoryUserDetailsManager();
-		
+		var bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		var user = User.withUsername("John")
-				.password("12345")
+				.password(bCryptPasswordEncoder.encode("12345"))
 				.authorities("read")
 				.build();
 		
 		userDetailsService.createUser(user);
 		
+		System.out.println(user.getPassword());
 		return userDetailsService;
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
+	}
+	
+//	public AuthenticationManager aunAuthenticationManager(UserDeta)
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.anyRequest().permitAll();
+		return http.build();
 	}
 }
